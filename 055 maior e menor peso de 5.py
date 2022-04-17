ec9abb99ece645b4c995e0877dc24c684094f399
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 11:24:47 -0300

    Enunciado e comentário inclusos.

--- 055 maior e menor peso de 5.py
+++ 055 maior e menor peso de 5.py
@@ -1,9 +1,12 @@
+# 055 Faça um programa que leia o peso de cinco pessoas.
+# No final, mostre qual foi o maior e o menor peso lidos.
+
 maior = 0 # A variavel recebe valor zero.
 menor = 0
 for i in range(1, 6):
     peso = float(input('Digite o {} pessoa: '.format(i))) # Aqui faz a leitura do primeiro laço.
     if i == 1: # Aqui verifica se o primeiro laço é igual a 1
-        maior = peso # Sendo igual a 1, ele atribui o valor as duas variavei
+        maior = peso # Sendo igual a 1, ele atribui o valor as duas variaveis
         menor = peso # maior e menor
     else: # no segundo laço, começa a atribuir o valor da variavel peso as variaveis maior e menor.
         if peso > maior:  # nesses dois if, atribui-se os valores maiores e menores
