037540011c7d3385121e3978a1bc54ff01d402b3
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 20:30:38 -0300

    1 versão

--- 057 leia sexo com while.py
+++ 057 leia sexo com while.py
@@ -2,6 +2,7 @@
 # Caso esteja errado, peça a digitação novamente
 # até ter um valor correto
 
+
 sexo = ''
 while sexo != 'M' and sexo != 'F':
     sexo = str(input('Digite o sexo [M/F]. ')).upper()
