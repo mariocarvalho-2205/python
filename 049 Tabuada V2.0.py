b3a9ed727746ecfbee98b0f05782a28a44509350
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 09:28:52 -0300

    inclusão do enunciado

--- 049 Tabuada V2.0.py
+++ 049 Tabuada V2.0.py
@@ -1,4 +1,6 @@
-# 049 - Tabuada V 2.0
+# Exercício Python 49: Refaça o DESAFIO 9, mostrando a
+# tabuada de um número que o usuário escolher,
+# só que agora utilizando um laço for.
 num = int(input('Digite um numero: '))
 for c in range(0, 10):
     print('{:2} x {:2} = \033[31m{:2}\033[m'.format(num, c + 1, (num*c)))
