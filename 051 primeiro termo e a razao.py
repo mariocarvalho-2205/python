743c88a336721fd40363731b740661ed2f43686d
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 10:33:43 -0300

    aplicação de soma a variavel decimo (regra de expressão)

--- 051 primeiro termo e a razao.py
+++ 051 primeiro termo e a razao.py
@@ -1,15 +1,15 @@
-# 051 - Progressão AritmeticaDesenvolva um programa que leia o primeiro termo e a razão de uma PA.
+# 051 - Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
 # No final, mostre os 10 primeiros termos dessa progressão.
+
 print('=' * 20)
 print('10 TERMOS DE UMA PA')
 print('=' * 20)
-primeiro = int(input('Primeiro elemento: '))
+primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
-#n = int(input('Quantos elementos: '))
-decimo = primeiro + (10 - 1) * razao
-#ultimo = primeiro + (n - 1) * razao
-#ultimo += 1
-#for var in range(primeiro, ultimo, razao):
-for c in range(primeiro, decimo + razao, razao):
+decimo = (primeiro + (10 - 1) * razao) + razao # fiz a formula ja incluindo a
+                                               # soma da razão na variavél usando
+                                               # as regras das expressões aritmeticas.
+
+for c in range(primeiro, decimo, razao):
     print(c, end=' -> ')
 print('ACABOU!')
