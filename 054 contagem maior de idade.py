bdd75fe27c2a224601d240a528adf28e1f69d039
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 11:11:11 -0300

    Enunciado e comentário inclusos. usando lista ou variavel.

--- 054 contagem maior de idade.py
+++ 054 contagem maior de idade.py
@@ -1,3 +1,6 @@
+# Crie um programa que leia o ano de nascimento de sete pessoas. No final,
+# mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
 import datetime
 ano = datetime.date.today().year
 
@@ -5,17 +8,20 @@ from datetime import date
 atual = date.today().year
 maior = 0
 menor = 0
-#maior = []
-#menor = []
-for c in range(1, 7):
+
+listamaior = [] # usando lista para somar no lugar de variavel.
+listamenor = []
+
+for c in range(1, 8):
     nasc = int(input('Em que ano a {} pessoa nasceu?  '.format(c)))
     if (atual - nasc) >= 21:
         maior += 1
-        #maior.append(nasc)
+        listamaior.append(nasc)
     else:
         menor += 1
-        #menor.append(nasc)
+        listamenor.append(nasc)
 print('Temos {} pessoas maior de idade, e {} menor de idade'.format(maior, menor))
-#print('Temos {} pessoas maior de idade, e {} menor de idade'.format(len(maior), len(menor)))
 
-#print(atual - nasc, ano, ano - nasc)
+print('Temos {} pessoas maior de idade, e {} menor de idade'.format(len(listamaior), len(listamenor)))
+
+
