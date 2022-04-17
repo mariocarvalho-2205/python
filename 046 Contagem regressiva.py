ac590f387914b75452b386de0f92792d027b878c
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-17 09:09:27 -0300

    comentario criado para o range

--- 046 Contagem regressiva.py
+++ 046 Contagem regressiva.py
@@ -3,7 +3,7 @@ import emoji
 print('*' * 45)
 print('Preparem-se para a contagem para os fogos!!!')
 print('*' * 45)
-for c in range(10, 0, -1):
+for c in range(10, -1, -1): # conceito - range(inicio, fim, salto)
     print(c)
     time.sleep(0.5)
 print(emoji.emojize('\033[32m:fireworks:\033[m' * 10))
