824bcea63c623ae15eada66f0b83891f378337d2
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-19 09:21:28 -0300

    Exercicio corrigido. utilizando variavel booleano

--- 058 Jogo da advinhação com while.py
+++ 058 Jogo da advinhação com while.py
@@ -6,12 +6,30 @@
 import random
 import time
 
-computador = random.randint(1, 4)
+computador = random.randint(0, 10)
 print('-_-' * 20)
-print('Vou pensar um um numero entre 0 a 10. Tente advinhar? ')
+print('Sou seu computador... Acabei de pensar em um numero entre 0 e 10. ')
+print('Será que você consegue advinhar?')
 print('-_-' * 20)
+acertou = False # A variavel recebe valor booleano negativo
+palpite = 0
+while not acertou: # Aqui o laço verifica se booleano é verdadeiro.
+    jogador = int(input('Em que numero eu pensei? '))
+    print('Hummm...')
+    palpite += 1
+    time.sleep(0.5)
+    if jogador == computador:
+        acertou = True # Se a condição entre jogador e computador for verdadeira,
+                       # A variavel acertou, recebe booleano verdadeiro.
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez. ')
+print('Acertou com {} palpites.'.format(palpite))
 
-tentativas = 0
+
+'''tentativas = 0
 jogador = 0
 
 while jogador != computador:
@@ -19,9 +37,13 @@ while jogador != computador:
     tentativas += 1
     print('Processando!!!')
     time.sleep(0.5)
-    if jogador == computador:
+    if jogador < computador:
+        print('Mais... Tente mais uma vez.')
+    elif jogador > computador:
+        print('Menos... Tente mais uma vez.')
+    elif jogador == computador:
         print('Parabes!! Você tentou \033[32m{}\033[m vezes até acertar. '.format(tentativas))
         print('O numero que eu escolhi foi o \033[32m{}\033[m e voçê o \033[32m{}\033[m'.format(computador, jogador))
     else:
         print('Que pena, você escolheu \033[31m{}\033[m, e eu escolhi \033[31m{}\033[m'.format(jogador, computador))
-        print('Tente de novo!!')
+        print('Tente de novo!!')'''
