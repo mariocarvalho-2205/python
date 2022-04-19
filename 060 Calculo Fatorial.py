08ed7d2d28c394d11da85bcc47965400f27c04ef
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-19 10:28:08 -0300

    Exercicio efetuado. Desafio: Fazer usando for.

---
+++ 060 Calculo Fatorial.py
@@ -0,0 +1,20 @@
+#060 - Faça um programa que leia um numero qualquer e mostre o seu fatorial
+# 5! = 5 · 4 · 3 · 2 · 1= 120
+
+# primeira forma
+'''from math import factorial
+num = int(input('Digite um numero para calcular o seu Fatorial: '))
+f = factorial(num)
+print('O fatorail de {} é {}'.format(num, factorial(int(num))))'''
+
+# Segunda forma
+n = int(input('Digite um numero para calcular o seu Fatorial: '))
+c = n
+f = 1
+print('Calculando {}! ='.format(n), end=' ')
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='') # usando condicional dentro de um print.
+    f *= c
+    c -= 1
+print('{}'.format(f))
