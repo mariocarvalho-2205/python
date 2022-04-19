38671b0e275ec812d32666a90049d2fd839ec5fa
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-19 08:55:22 -0300

    Exercicio corrigido.

--- 057 leia sexo com while.py
+++ 057 leia sexo com while.py
@@ -3,9 +3,31 @@
 # até ter um valor correto
 
 
-sexo = ''
+sexo = str(input('Digite o sexo [M/F]. ')).upper().strip()[0] # .upper para maiuscula,
+                                                                  # strip para remover espaços
+                                                                  # [0] para pegar so a primeira letras
+                                                                  # usando fatiamento.
+while sexo not in 'MmFf': # dessa forma a variavel já é validada.
+    sexo = str(input('Dados invalidos. Por favor digite o sexo [M/F]. ')).upper().strip()[0]
+
+print('Sexo {} digitado com sucesso. '.format(sexo))
+
+
+
+
+
+'''sexo = 
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Digite o sexo [M/F]. ')).upper()
-    if sexo != 'M' and sexo != 'F':
-        print('Digite a opção correta. ')
-print('Acabou!')
+    sexo = str(input('Digite o sexo [M/F]. ')).upper().strip()[0] # .upper para maiuscula,
+                                                                  # strip para remover espaços
+                                                                  # [0] para pegar so a primeira letras
+                                                                  # usando fatiamento.
+    if sexo == 'M':
+        print('Sexo Masculino digitado com sucesso. ')
+    elif sexo == 'F':
+        print('Sexo Feminino digitado com sucesso. ')
+    #elif sexo != 'M' and sexo != 'F':
+    else:
+        print('Dados invalidos. Por favor, informe seu sexo. ')
+
+print('Acabou!')'''
