0990defcb206c2f23d61abcfae6dc9e7d8a1cd82
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-19 09:39:16 -0300

    Exercicio corrigido.

--- 059 dois numeros e menu.py
+++ 059 dois numeros e menu.py
@@ -8,11 +8,36 @@ Seu programa deverá realizar a operação solicitada em cada caso.'''
 
 num1 = int(input('Digite o 1 valor: '))
 num2 = int(input('Digite o 2 valor: '))
-opcao = ''
-while opcao != '5':
-    opcao = str(input('''Escolha o que deseja fazer:
- [1] Somar
- [2] Multiplicar
- [3] Maior
- [4] Novos Numeros
- [5] Sair do Programa: '''))
+opcao = 0
+
+while opcao != 5:
+  opcao = int(input('''\nEscolha o que deseja fazer:
+[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos Numeros
+[5] Sair do Programa:
+Digite sua opção: '''))
+  if opcao == 1:
+    soma = num1 + num2
+    print('Opção 1 - O resultado da soma entre {} + {} é {}'.format(num1, num2, soma))
+  elif opcao == 2:
+    multi = num1 * num2
+    print('Opção 2 - A multiplicação enter {} x {} é {}'.format(num1, num2, multi))
+  elif opcao == 3:
+    if num1 > num2:
+      maior = num1
+    elif num2 > num1:
+      maior = num2
+      print('Opção 3. Entre {} e {} o maior numero é {}'.format(num1, num2, maior))
+    else:
+      print('Os numeros são iguais.')
+  elif opcao == 4:
+    print('Digite novos numeros: ')
+    num1 = int(input('Digite o 1 valor: '))
+    num2 = int(input('Digite o 2 valor: '))
+  elif opcao == 5:
+    print('O programa será finalizado.')
+  else:
+    print('Digite uma opção valida.')
+print('Fim do programa. Volte Sempre!!')
