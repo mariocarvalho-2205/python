84f00f1f586fc201fd2645d60452247f37b95ca7
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-23 09:39:30 -0300

    Feito os comentarios para analise

--- 062 Progressao aritmetica while com finalização.py
+++ 062 Progressao aritmetica while com finalização.py
@@ -6,11 +6,18 @@ print('Os 10 termode de uma PA.')
 print('-*' * 20)
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a Razão: '))
-decimo = (primeiro + (10 - 1) * razao) + razao
-c = 0
-while c <= primeiro:
-    if c < 10:
-        print(primeiro, end=' -> ')
-        primeiro += razao
-    c += 1
+termo = primeiro + razao # O primeiro termo recebe a soma da razao
+mais = 10 # variavel começa com 10 para repetir o loop inicial
+c = 1 # Contator
+total = 0 # vai receber o valor somado da variavel mais
+
+while mais != 0: # nesse laço e verificado a condição - Se for diferente de 0, continua.
+    total += mais # A variavel soma recebe a soma da variavel mais para continuar o loop.
+    while c <= total: # Esse laço e responsavél por fazer o loop do termo de PA.
+        print(termo, end=' -> ')
+        termo += razao # A var TERMO e somada a var RAZAO
+        c += 1 # O contador recebe o incremento de 1 em cada laço
+    print('Pausa!!')
+    mais = int(input('Quantos termos voce quer mostrar mais? ')) # A var MAIS recebe o valor para continuar.
+print('Progressao finalizada com {} termos.'.format(total))
 print('Acabou!!!')
