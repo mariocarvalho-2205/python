f5480a6003bca75ed22ccf2e826bb1f31c6c77c8
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-04-24 09:06:45 -0300

    exercicio modificado e tocando o mp3. Precisa melhorar a finalização.

--- 020 Reproduzir mp3.py
+++ 020 Reproduzir mp3.py
@@ -3,11 +3,23 @@ from pygame import mixer
 #song = '020 Reproduzir mp3.mp3'
 pygame.init()
 mixer.init()
-mixer.music.load(nome do arquivo.mp3)
+mixer.music.load('O TECLADO dos MEUS SONHOS para TRABALHAR ficou MAIS COMPACTO!!   MX KEYS MINI.mp3')
 mixer.music.set_volume(0.7)
 mixer.music.play()
 
 while True:
+    print('press ''p'' to pause, ''r'' to resume')
+    print('Press ''e'' para sair do programa')
+    query = input(' ')
+
+    if query == 'p':
+        mixer.music.pause()
+    elif query == 'r':
+        mixer.music.unpause()
+    elif query == 'e':
+        mixer.music.stop()
+        break
+
 
 
 '''tela = pygame.display.set_mode((400, 400), 0, 32)
