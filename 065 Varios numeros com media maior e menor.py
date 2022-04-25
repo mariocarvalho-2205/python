fbf21bb673d52e515c089f8f9e8d496a9249b8c5
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-04-25 10:46:56 -0300

    Falta so realizar a condição de maior e menor.

--- 065 Varios numeros com media maior e menor.py
+++ 065 Varios numeros com media maior e menor.py
@@ -2,22 +2,29 @@
 # No final da execução, mostre a media entre todos os valores e qual foi
 # o maior e o menor valor lido. O programa deve perguntar ao usuario se ele quer
 # continuar a digitar valores.
-total = maior = menor = cont = 0
-continuar = 'S'
-while continuar == 'S':
+total = 0
+maior = 0
+menor = 0
+cont = 0
+
+continuar = True
+
+
+while continuar == True:
     num = int(input('Digite um numero: '))
     total += num
+    maior = num
+    menor = num
+    continuar = str(input('Quer continuar [S/N]? ')).strip().upper()
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+    else:
+        if continuar == 'S':
+            continuar = True
     cont += 1
-    if cont == 1: # Sempre que o contador for igual a 1, prosegue a condicional
-        maior = menor = num # atribuindo valor da variavel num as variaveis maior e menor.
-    else: # Aqui verifica a condicional de maior e menor
-        if num > maior:
-            maior = num # Aqui é atribuido valor se var-num for maior.
-        if num < menor:
-            menor = num # Aqui é atribuido valor se var-num for menor.
-    continuar = str(input('Quer continuar [S/N]? ')).strip().upper() # A validação tem que estar
-        # dentro do laço while.
 media = total / cont
-print('Foram digitados {} numeros, e a media entre eles é {:.2f}'.format(cont, media))
+print('Foram digitados {} numeros, e a media entre eles é {}'.format(cont, media))
 print('Dos numeros digitados, o maior foi {}, e o menor foi {}.'.format(maior, menor))
 print('Fim!!!')
