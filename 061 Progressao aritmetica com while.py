f1331c0ca35443f87e2bc5fd90f07ecab2a8018d
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-26 10:10:40 -0300

    Atualizado, corrigido e comentado.

--- 061 Progressao aritmetica com while.py
+++ 061 Progressao aritmetica com while.py
@@ -6,11 +6,13 @@ print('Os 10 termode de uma PA.')
 print('-*' * 20)
 primeiro = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a Razão: '))
-decimo = (primeiro + (10 - 1) * razao) + razao
+# decimo = (primeiro + (10 - 1) * razao) + razao -> dessa forma foi usada a formula matematica.
 c = 0
-while c <= primeiro:
-    if c < 10:
-        print(primeiro, end=' -> ')
-        primeiro += razao
+while c < 10: # Repete o laço enquanto o contador for menor que 10
+
+    print(primeiro, end=' -> ')
+    primeiro += razao # Aqui esta sendo usada a logica,
+                      # somando o primeiro termo com a razão em cada laço
+                      # substituindo a formula matematica.
     c += 1
 print('Acabou!!!')
