04681c62f93a29226251e7f5443b5dd48b69b779
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-27 16:30:27 -0300

    Exercicio feito com laço while usando o comando break. Utilizando todos os operadores.

---
+++ 067 Tabuada V4.0.py
@@ -0,0 +1,28 @@
+print('-*' * 20)
+print('-' * 12, 'Tabuada V4.0:', '-' * 12)
+print('-*' * 20)
+
+while True:
+    print('-' * 40)
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n <= 0:
+        break
+    op = str(input('''Qual operador voce quer usar? 
+[+]Somar 
+[-]Subtrair 
+[*]Multiplicar 
+[/]Dividir: ''')).upper().strip()
+    print('-' * 40)
+    c = 1
+    while c <= 10:
+        if op == '+':
+            print(f'{n:2} + {c:2} = {n + c:2}')
+        elif op == '-':
+            print(f'{c + (n - 1):2}  - {n:2} = {c - 1:2}')
+        elif op == '*':
+            print(f'{n:2}  x {c:2} = {n * c:2}')
+        elif op == '/':
+            print(f'{n * c:2}  / {n:2}  = {(n * c) / n:.0f}')
+        c += 1
+print('O programa encerrou porque foi digitado um valor negativo.')
+
