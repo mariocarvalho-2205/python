820d75a91c30e75081b58eb2586585156581d936
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-04-27 13:38:40 -0300

    Atualizado, corrigido e comentado. OBS: As duas versões funcionam.

--- 064 - varios numeros.py
+++ 064 - varios numeros.py
@@ -3,14 +3,24 @@
 # que é a condição de parada. No final, mostre quantos numeros foram digitados,
 # e qual foi a soma entre eles (desconsiderando o flag).
 
-num = 0
-soma = 0
-cont = 0
+
+# Minha solução foi usando o if
+num = soma = cont = 0
 
 while num != 999:
-    num = int(input('Digite um numero: '))
+    num = int(input('Digite um numero diferente de [999 para encerrar]: '))
     if num != 999:
         cont += 1
         soma += num
 
 print('Foram lidos {} numeros, e a soma entre eles é {}.'.format(cont, soma))
+
+# A correção foi dessa forma.
+n = s = c = 0
+print('2 Versão. ')
+n = int(input('Digite um numero. [999 para encerrar]: '))
+while n != 999:
+    s += n
+    c += 1
+    n = int(input('Digite um numero. [999 para encerrar]: '))
+print('Foram lidos {} numeros e a soma entre eles é {}.'.format(c, s))
