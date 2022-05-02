3debecf6bfda126080dc74ba26f26807a5127b2c
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-01 21:49:34 -0300

    Programa finalizado. Corrigido

--- 068 Jogo do par ou impar.py
+++ 068 Jogo do par ou impar.py
@@ -7,6 +7,44 @@ from random import randint
 print('*' * 25)
 print('VAMOS JOGAR PAR OU IMPAR?')
 print('*' * 25)
+cont = 0
+vitorias = 0
+while True:
+    num_jogador = int(input('Diga um valor: '))
+    computador = randint(0, 11)
+    total = num_jogador + computador
+    cont += 1
+    opc_jogador = ' '
+    while opc_jogador not in 'PI':
+        opc_jogador = str(input('Par ou Impar [P/I] ')).upper().strip()[0]
+    print(f'Você jogou \033[32m{num_jogador}\033[m e o computador jogou {computador}. total de {total} ', end='')
+    print('DEU PAR' if total % 2 == 0 else 'DEU IMPAR')
+    if opc_jogador == 'P':
+        if total % 2 == 0:
+            print('\033[36mVocê Ganhou!\033[m')
+            vitorias += 1
+        else:
+            print(f'\033[31mVocê Perdeu! Eu ganhei com {cont} tentativas.\033[m')
+            break
+    elif opc_jogador == 'I':
+        if total % 2 == 1:
+            print('\033[36mVocê Ganhou!\033[m')
+            vitorias += 1
+        else:
+            print(f'\033[31mVocê Perdeu! Eu ganhei com {cont} tentativas.\033[m')
+            break
+    print('Vamos jogar novamente...')
+    print('_-*' * 10)
+print('_-' * 15)
+print(f'GAME OVER! Você venceu {vitorias} vezes.')
+
+
+
+
+'''from random import randint
+print('*' * 25)
+print('VAMOS JOGAR PAR OU IMPAR?')
+print('*' * 25)
 computador = randint(1, 10)
 cont = 0
 vitorias = 0
@@ -35,7 +73,7 @@ while True:
     print('Vamos jogar novamente...')
     print('_-*' * 10)
 print('_-' * 15)
-print(f'GAME OVER! Você venceu {vitorias} vezes.')
+print(f'GAME OVER! Você venceu {vitorias} vezes.')'''
 
 
 '''from random import choice
