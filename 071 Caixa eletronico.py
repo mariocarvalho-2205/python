d72b2e7d1282bbee0cbc2c82e890dba488a5fe09
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-04 10:11:38 -0300

    preciso praticar esse exercicio e tentar fazer outros modos

--- 071 Caixa eletronico.py
+++ 071 Caixa eletronico.py
@@ -6,4 +6,27 @@
 # OBS: Considere que o programa possui
 # cedulas de R$ 50, R$ 20, R$ 10 e R$ 1
 
+print('=' * 30)
+print(f'{"BANCO CARVALHO":^30}')
+print('=' * 30)
+valor = int(input('Quanto você quer sacar? R$ '))
+total = valor
+cedula = 50
+totalcedula = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        totalcedula += 1
+    else:
+        if totalcedula > 0:
+            print(f'total de {totalcedula} cedulas de R$ {cedula}')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 1
+        totalcedula = 0
+        if total == 0:
+            break
 
