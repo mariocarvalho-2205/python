50b45d3ab2515d9bacfec3a0b05a202d6caffbbf
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-18 22:56:28 -0300

    Programa finalizado, faltando correção

--- 076 lista de preços.py
+++ 076 lista de preços.py
@@ -3,3 +3,13 @@ produtos e seus respectivos preços, na sequencia.
 No final, mostre uma listagem de preços,
 organizando os dados em forma tabular.
 '''
+
+lista_produtos = (['Pao', 0.50], ['Cebola', 1.00], ['Açucar', 4.75], ['Feijão', 7.89], ['Arroz', 6.99])
+print('-' * 38)
+total = 0
+for produto, preço in lista_produtos:
+    print(f'{produto:.<30}R$  {preço:3.2f}')
+    total += preço
+print('-' * 38)
+print(f'Total {"R$ ":.>27}{total:.2f}')
+print('-' * 38)
