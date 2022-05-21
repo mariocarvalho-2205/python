9efd81ec71d6f05a176a22405227fcd31e89a120
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-21 08:56:11 -0300

    Programa finalizado,Corrigido. OBS: A coreção ta com bug.

--- 072 numero por extenso.py
+++ 072 numero por extenso.py
@@ -1,7 +1,10 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem de
 # zero a vinte.
 # Seu programa deverá ler um numero pelo teclado (entre 0 e 20) e mostra-lo por extenso
-tuplenum = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
+tuplenum = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco',
+            'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
+            'doze', 'treze', 'quatorze', 'quinze', 'dezesseis',
+            'dezesete', 'dezoito', 'dezenove', 'vinte')
 num = int(input('Digite um numero entre 0 e 20: '))
 
 while True:
@@ -14,3 +17,11 @@ print(f'Você digitou o numero {tuplenum[num]}')
 while num < 0 or num > 20:
     num = int(input('Tente novamente!! Digite um numero entre 0 e 20: '))
 print(f'Você digitou o numero {tuplenum[num]}')
+
+#Correção
+'''while True:
+    num = int(input('Digite um numero entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Voce digitou o numero {tuplenum[num]}')'''
