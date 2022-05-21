a902046f0bb3c3e6dac1ff22749a4b4e9085cd77
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-21 09:28:28 -0300

    Corrigido.

--- 074 tupla aleatoria.py
+++ 074 tupla aleatoria.py
@@ -1,13 +1,19 @@
 import random
-#aleatorio = []
+from random import randint
 
 aleatorio = [random.randint(0, 9) for c in range(0, 5)]
-#for c in range(0, 5):
-    #aleatorio.append(random.randint(0, 9))
 modificado = str(aleatorio)[1: -1].replace(', ', ' ')
-
 print(f'Os valores sorteados foram: {modificado}')
 print(f'O maior valor sorteado foi: {max(aleatorio)}')
 print(f'O menor valor sorteado foi: {min(aleatorio)}')
+
+sorteado = (randint(0, 10), randint(0, 10), randint(0, 10),
+            randint(0, 10), randint(0, 10))
+print('Os numeros soteados foram: ', end=' ')
+for c in sorteado:
+    print(f'{c} ', end=' ')
+print(f'\nO maior valor sorteado foi: {max(sorteado)}')
+print(f'O menor valor sorteado foi: {min(sorteado)}')
+
 
 
