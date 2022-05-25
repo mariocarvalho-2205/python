eac0884adf467ab9552e3433e4a9fce3815599fe
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-25 11:01:11 -0300

    em andamento

---
+++ 080 Lista ordenada sem sort().py
@@ -0,0 +1,7 @@
+'''
+080 - Crie umprograma onde o usuario possa digitar cinco valores numericos
+e cadastre-os em uma lista, ja na posição correta de inserção (sem usar o sort()).
+No final, mostre a lista ordenada na tela
+'''
+
+lista = []
