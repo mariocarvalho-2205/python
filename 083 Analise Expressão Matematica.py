00cf97b56d63ed431961fa4f7ee24954ed8b2f66
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-26 11:00:53 -0300

    Iniciado

---
+++ 083 Analise Expressão Matematica.py
@@ -0,0 +1,6 @@
+'''
+083 - Crie um programa onde o usuario digite uma expressão qualquer
+que use parenteses.
+Seu aplicativo deverá analisar se a expressão passada está com os
+parenteses abertos o fechados na ordem correta.
+'''
