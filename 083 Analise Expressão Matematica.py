e1811b1e83123c013c05faacdbb1aec68e86eb39
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-26 11:25:31 -0300

    Iniciado

--- 083 Analise Expressão Matematica.py
+++ 083 Analise Expressão Matematica.py
@@ -4,3 +4,11 @@ que use parenteses.
 Seu aplicativo deverá analisar se a expressão passada está com os
 parenteses abertos o fechados na ordem correta.
 '''
+exp = str(input('Digite a expressão: '))
+print('A expressão está correta' if exp.count('(') == exp.count(')')
+         else 'Sua expressão está errada!!')
+
+if exp.count('(') == exp.count(')'):
+    print('Sua expressão está correta!!!')
+else:
+    print('Sua expressão está errada!!!')
