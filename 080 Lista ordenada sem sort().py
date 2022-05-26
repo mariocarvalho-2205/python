6218292bad6da2257028401fdd5bc5ba814e7182
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-26 09:52:54 -0300

    Corrigido com Observações.

--- 080 Lista ordenada sem sort().py
+++ 080 Lista ordenada sem sort().py
@@ -6,5 +6,20 @@ No final, mostre a lista ordenada na tela
 
 lista = []
 
-for c in range(0, 5):
-    num = int(input('Digite um valor: '))
+for c in range(0, 5): # Cria a lista com laço de repetição
+    num = int(input('Digite um valor: ')) # Recebe o item a ser colocado na lista
+    if c == 0 or num > lista[-1]: # Aqui verifica o inicio da lista com o inicio do laço
+        lista.append(num) # Adiciona o item lido ao final da lista
+        print('Adicionado ao final da lista!')
+    else:
+        pos = 0 # Variavel ede criação da posição que sera
+                # #comparada com a posição da lista
+        while pos < len(lista): # Laço de verificação da posição com cada item na lista
+            if num <= lista[pos]:      # Verifica se o item digitado é menor
+                                       # que cada item da lista junto com a posição.
+                lista.insert(pos, num) # para ser adicionado antes do maior
+                print(f'Adicionado na posição {pos}...')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados foram {lista}')
