ebcbb10a668cbd71e1cf610636b8d587561c9640
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-27 10:23:35 -0300

    Corrigido.

--- 082 lista com pares e impares.py
+++ 082 lista com pares e impares.py
@@ -15,10 +15,10 @@ while True:
     if continar in 'N':
         break
 print(f'A lista completa é {lista}')
-for c in range(0, len(lista)):
-    if lista[c] % 2 == 0:
-        pares.append(lista[c])
-    else:
-        impares.append(lista[c])
+for c in range(0, len(lista)): # for c, v in enumerate(lista):
+    if lista[c] % 2 == 0:      #    if v % 2 == 0:
+        pares.append(lista[c]) #        pares.append(v)
+    else:                      #    elif v % 2 == 1:
+        impares.append(lista[c])#       impares.append(v)
 print(f'A lista de pares é {pares}')
 print(f'A lista de impares é {impares}')
