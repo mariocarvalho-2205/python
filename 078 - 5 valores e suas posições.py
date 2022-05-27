2062b9c625362e07876a2fc857651af3ae14c26e
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-05-27 09:39:12 -0300

    Corrigido.

--- 078 - 5 valores e suas posições.py
+++ 078 - 5 valores e suas posições.py
@@ -19,5 +19,35 @@ print(f'\nO menor numero digitado foi {min(lista)} na posição ', end=' ')
 for c, v in enumerate(lista):
     if v == min(lista):
         print(f'{c}...', end=' ')
+print()
+# Correção:
+listanum = []
+mai = 0
+men = 0
+
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('-=' * 30)
+print(f'Os numeros digitados foram {listanum}')
+print(f'O maior numero digitado foi {mai} na posição ', end=' ')
+
+for c, v in enumerate(listanum):
+    if v == mai:
+        print(f'{c}...', end=' ')
+print(f'\nO menor numero digitado foi {men} na posição ', end=' ')
+for c, v in enumerate(listanum):
+    if v == men:
+        print(f'{c}...', end=' ')
+
+
+
+
 
 
