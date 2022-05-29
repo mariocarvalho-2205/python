bada7214d57b76d8ba468e3ad2e50a2843b3b119
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-29 11:00:43 -0300

    exe086 - Exercicio iniciado.

--- 086 Matrix 3x3.py
+++ 086 Matrix 3x3.py
@@ -3,3 +3,14 @@
 No final mostre a matriz na tela, com a formatação correta.
 '''
 matriz = []
+
+for i in range(0, 3):
+    linha = []
+    for j in range(0, 3):
+        linha.append(int(input('Digite um numero: ')))
+    matriz.append(linha)
+print(matriz)
+for linha in range(3):
+    for coluna in range(3):
+        print(matriz[linha][coluna], end=' ')
+    print()
