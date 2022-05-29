ab8bffdd13d347ce9d15d4f74b55856bb2ba188f
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-28 21:51:56 -0300

    exe084 - inicio

---
+++ 084 listas nome e peso.py
@@ -0,0 +1,7 @@
+'''
+084 - Crie um programa queleia nome e peso de varias pessoas, guardando tudo em uma lista.
+No final, mostre:
+A-) Quantas pessoas foram cadastradas.
+B-)Uma listagem com as pessoas mais pessadas
+C-)Uma listagem com as pessoas mais leves
+'''
