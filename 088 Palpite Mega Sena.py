23e512c9c21f8f962c58406528f3c70aa37f1ec5
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-05-30 21:24:32 -0300

    exe088 - Iniciado.

---
+++ 088 Palpite Mega Sena.py
@@ -0,0 +1,5 @@
+'''
+088 - Faça um programa que ajude um jogador da Mega Sena a criar palpites.
+O programa vai perguntar quantos jogos serão gerador e vai sortear 6 numeros
+entre 1 e 60 para cada jogo, cadastrando em uma lista composta.
+'''
