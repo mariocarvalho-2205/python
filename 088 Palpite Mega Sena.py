a4698082b8d3b31d7e8c0ec03838d9179950c53e
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-06-01 08:39:18 -0300

    Finalizado

--- 088 Palpite Mega Sena.py
+++ 088 Palpite Mega Sena.py
@@ -3,3 +3,26 @@
 O programa vai perguntar quantos jogos serão gerador e vai sortear 6 numeros
 entre 1 e 60 para cada jogo, cadastrando em uma lista composta.
 '''
+import time
+from random import randint
+from time import sleep
+print('-=' * 15)
+print(f'{"Joga na mega sena":^30}'.upper())
+print('-=' * 15)
+palipte = []
+
+jogos = int(input('Quantos jogos você quer que eu sortei? '))
+for c in range(0, jogos):
+    temp = []
+    for j in range(0, 6):
+        temp.append(randint(1, 60))
+    palipte.append(temp)
+print('-=' * 3, f'sorteando {jogos} jogos!'.upper(), '-=' * 3)
+for j in range(len(palipte)):
+    print(f'Jogo {j + 1}: {palipte[j]}')
+    time.sleep(0.5)
+print('-=' * 4, f'{"< boa sorte! >":^3}'.upper(), '-=' * 4)
+
+
+
+
