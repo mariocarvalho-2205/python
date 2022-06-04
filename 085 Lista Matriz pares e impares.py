2a1090564cf15923227a9ab46d447c8dc91e27b4
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-06-04 07:39:23 -0300

    Finalizado comentado.

--- 085 Lista Matriz pares e impares.py
+++ 085 Lista Matriz pares e impares.py
@@ -3,12 +3,13 @@
 cadastre-os em uma lista unica que mantenha os valores separados pares e impares.
 No final, mostre os valores pares e impares em ordem crescente.
 '''
-numeros = [[],[]]
+numeros = [[], []] # para a criação do exercicio, foi necessaria criar listas dentro
+                   # da lista principal
 for c in range(1, 8):
     num = int(input(f'Digite o {c}o numero: '))
     if num % 2 == 0:
         numeros[0].append(num)
     if num % 2 == 1:
         numeros[1].append(num)
-print(f'Os numeros pares foram {sorted(numeros[0])}')
+print(f'Os numeros pares foram {sorted(numeros[0])}') # pode ser ordenado antes de exibir
 print(f'Os numeros impares foram {sorted(numeros[1])}')
