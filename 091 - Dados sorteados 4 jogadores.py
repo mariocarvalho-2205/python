6ffe33465a7f3898c3c713969c30d94474670ba2
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-06-11 11:21:12 -0300

    Exercicio 091 - iniciado

--- 091 - Dados sorteados 4 jogadores.py
+++ 091 - Dados sorteados 4 jogadores.py
@@ -5,9 +5,18 @@ No final, coloque esse dicionario em ordem, sabendo que o vencedor tirou o
 maior numero no dado.
 '''
 from random import randint
+from time import sleep
 jogadores = dict()
 jogadores2 = {'Jogador 1': randint(1, 6), 'Jogador 2': randint(1, 6)}
+print('Valores Sorteados:')
 for c in range(0, 4):
     jogadores[f'jogador {c + 1}'] = randint(1, 6)
-print(jogadores)
-print(jogadores2)
+for k, v in jogadores.items():
+    print(f'\tO {k} tirou {v}')
+    sleep(.8)
+print('Ranking dos Jogadores:')
+for n, c in sorted(jogadores, key = jogadores.get, reverse=True):
+    print(n + 1, c, jogadores[c])
+
+
+
