9124e885edbbd676aa277936c7a2636f721f8da0
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-06-12 19:28:44 -0300

    Exe-092 commit finalizado para correção

--- 092 nome ano nasc carteira de trabalho.py
+++ 092 nome ano nasc carteira de trabalho.py
@@ -4,3 +4,22 @@ cadastre-os (com idade) em um dicionario, se por acaso a CTPS for diferente de Z
 o dicionario recebera tambem o ano de contratação e o salario. Calcule e acrescente,
 alem da idade, com quantos anos a pessoa vai se aposentar.
 '''
+from datetime import datetime
+dados = dict()
+dados['Nome'] = str(input('Nome: ')).capitalize().strip()
+nasc = int(input('Ano de Nascimento: '))
+dados['Idade'] = datetime.now().year - nasc
+dados['CTPS'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['CTPS'] != 0:
+    dados['Contratação'] = int(input('Ano de Contratação: '))
+    dados['Salario'] = float(input('Salario: '))
+anos_trabalhados = 35 - (datetime.now().year - dados['Contratação'])
+dados['Aposentadoria'] = dados['Idade'] + anos_trabalhados
+print('-=' * 15)
+for k, c in dados.items():
+    print(f'{k} tem o valor {c}')
+
+
+
+
+
