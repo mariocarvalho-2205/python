7cc71f9ef83f70d92c6f1de91dcd2472d73ec891
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-06-12 18:09:03 -0300

    Aula 19 commit final aguardando correção

--- 091 - Dados sorteados 4 jogadores.py
+++ 091 - Dados sorteados 4 jogadores.py
@@ -7,7 +7,7 @@ maior numero no dado.
 from random import randint
 from time import sleep
 jogadores = dict()
-jogadores2 = {'Jogador 1': randint(1, 6), 'Jogador 2': randint(1, 6)}
+
 print('Valores Sorteados:')
 for c in range(0, 4):
     jogadores[f'jogador {c + 1}'] = randint(1, 6)
@@ -19,5 +19,17 @@ for i, c in enumerate(sorted(jogadores, key=jogadores.get, reverse=True)):
     print(f'\tO {i + 1}o lugar foi o {c} com {jogadores[c]}')
     sleep(.8)
 
+# segunda versão
 
+jogadores2 = {'Jogador 1': randint(1, 6), 'Jogador 2': randint(1, 6),
+              'Jogador 3': randint(1, 6), 'Jogador 4': randint(1, 6),
+              'Jogador 5': randint(1, 6), 'Jogador 6': randint(1, 6)}
+print('Valores Sorteados:')
+for k, v in jogadores2.items():
+    print(f'\tO {k} tirou {v}')
+    sleep(.8)
+print('Ranking dos Jogadores:')
+for i, c in enumerate(sorted(jogadores2, key=jogadores2.get, reverse=True)):
+    print(f'\tO {i + 1}o lugar foi o {c} com {jogadores2[c]}')
+    sleep(.8)
 
