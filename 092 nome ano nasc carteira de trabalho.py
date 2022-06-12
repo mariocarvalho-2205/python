91c0f4da3065fbe073c801a6aede984ed3767c62
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-06-12 18:19:39 -0300

    Exe-092 commit inicial

---
+++ 092 nome ano nasc carteira de trabalho.py
@@ -0,0 +1,6 @@
+'''
+092 - Crie um programa que leia nome, ano de nascimento e carteira de trabalho e
+cadastre-os (com idade) em um dicionario, se por acaso a CTPS for diferente de ZERO,
+o dicionario recebera tambem o ano de contratação e o salario. Calcule e acrescente,
+alem da idade, com quantos anos a pessoa vai se aposentar.
+'''
