a8a3752e04abe3f2ed43f0a792d860910f0bae88
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-06-13 10:16:44 -0300

    Exercicio 094- Iniciado.

---
+++ 094 Unindo dicionario e lista.py
@@ -0,0 +1,9 @@
+'''
+094- Crie um programa que leia nome, sexo e idade de varias pessoas,
+guardando os dados de cada pessoa em um dicionario e todos os
+dicionarios em uma lista. No final, mostre:
+A-) Quantas pessoas foram cadastradas
+B-) A media de idade do grupo
+C-) Uma lista com todas as mulheres
+D-) Uma lista com todas as pessoas com idade acima da media.
+'''
