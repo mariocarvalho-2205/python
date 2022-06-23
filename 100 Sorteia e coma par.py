75194a2dac88bd0330ac6747c84e7c909d1ccd63
mariocarvalho2205 <mario.carvalho.devpython@gmail.com>
2022-06-22 21:01:27 -0300

    100: Finalizado e corrigido

--- 100 Sorteia e coma par.py
+++ 100 Sorteia e coma par.py
@@ -6,16 +6,31 @@ a segunda função vai mostrar a soma entre todos os valores pares
 sorteados pela função anterior.
 '''
 from random import randint
+from time import sleep
 
 
 def sorteia(lista):
+    print('-=' * 30)
+    print(f'Sorteando 5 valores: ', end='', flush=True)
+    sleep(0.5)
     for c in range(0, 5):
         n = randint(0, 10)
         lista.append(n)
+        print(f'{c}', end=' ')
+        sleep(0.5)
+    print(f'Pronto!!')
 
-#def somapar():
+
+def somapar(par):
+    pares = 0
+    for c in numeros:
+        if c % 2 == 0:
+            pares += c
+    print(f'Somando os valores pares de {numeros}, temos {pares}')
+    print('-=' * 30)
+
+
 numeros = list()
-
 sorteia(numeros)
-print(numeros)
+somapar(numeros)
 
