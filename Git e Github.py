62eb42f6227825466e689b56df44eafcfa30c04d
mariocarvalho-2205 <mario.carvalho.devpython@gmail.com>
2022-06-24 10:46:58 -0300

    Comando (cmd e git)

---
+++ Git e Github.py
@@ -0,0 +1,37 @@
+'''
+Git e Github
+
+Comandos basicos:
+1-) dir ou ls: É usado para listar as pastas e arquivos que contem no diretorio.
+
+2-) cd : é usado para acessar pastas e diretorios.
+ex: cd /d/pasta/pasta\ com\ espaco/
+        OBS: usa-se barra invertida para
+        acessar pastas com mais de uma palavra.
+
+cd / volta para a base do diretorio git
+cd pasta - entra na pasta contida no diretorio
+cd /unidade - entra na unidade escolhida (disco rigido, pendrive, etc)
+cd /unidade/pasta - entra na pasta especifica
+cd .. - volta um nivel da pasta local
+        ex:  /d/artigos/pastas/nome
+        cd.. - vai para /d/artigos/pastas
+        cd.. - vai para /d/artigos
+OBS: No cdm basta digitar diretorio. Ex. d: e: c:
+
+3-) mkdir: é usado para criar pasta no diretorio
+ex: mkdir nome da pasta  # Dica: Evitar usar nomes grandes de pastas
+
+4-) echo >: usado para escrever na tela a palavra digitada apos o comando
+echo nome.extensão - cria arquivo na extensao
+Ex no cmd: echo hello > hello.txt
+
+5-) del + nome do arquivo no cmd: Apaga apenas arquivos dentro da pasta.
+    obs: se estiver fora da pasta, del nome da pasta apaga todos os arquivos dentro dela
+rmdir + nome da pasta /S /Q: apaga a pasta com tudo dentro
+# no git: rm -rf nome da pasta: apaga a pasta com arquivos dentro
+rm nome do arquivo: apaga o arquivo dentro da pasta
+
+
+6-) cls no cmd / clear no git: Limpa a tela
+'''
