5476424b0bdbfd8c12b91b11f72b50a0b6446a17
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-06-26 23:09:20 -0300

    Exe102- commit inicial

--- 102 Função Fatorial.py
+++ 102 Função Fatorial.py
@@ -4,3 +4,14 @@ o primeiro que indique o numero a calcular e o outro chamado show, que será um
 valor logico(opcional) indicado se será mostrado ou não na tela o processo de calculo
 fatorial.
 '''
+
+def fatorial(n, show=False):
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(f'{c} x ', end='')
+        f *= c
+    return f
+print(fatorial(5, show=True))
+
+
