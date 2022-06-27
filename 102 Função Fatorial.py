e0571046ac6e96c1f47135781be994696179f737
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-06-26 22:48:12 -0300

    Exe102- commit inicial

---
+++ 102 Função Fatorial.py
@@ -0,0 +1,6 @@
+'''
+101- Crie um programa que tenha uma função fatorial() que receba dois parametros:
+o primeiro que indique o numero a calcular e o outro chamado show, que será um
+valor logico(opcional) indicado se será mostrado ou não na tela o processo de calculo
+fatorial.
+'''
