287a3daefc294813ef701f2aa66fee0408e15f66
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-06-27 22:13:48 -0300

    Exe102- Corrigido

--- 102 Função Fatorial.py
+++ 102 Função Fatorial.py
@@ -5,13 +5,31 @@ valor logico(opcional) indicado se será mostrado ou não na tela o processo de 
 fatorial.
 '''
 
+
 def fatorial(n, show=False):
+    '''
+    >= Faz o calculo fatorial do numero escolhido.
+    :param n: Variavel recebe o numero digitado pelo usuario.
+    :param show: True> exibe o calculo do fatorial. False> exibe apenas o resultado.
+    :return: retora a função com o resultado.
+    '''
     f = 1
+    print(f'Calculando {n}! = ', end='')
     for c in range(n, 0, -1):
         if show:
-            print(f'{c} x ', end='')
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
         f *= c
     return f
-print(fatorial(5, show=True))
+
+
+# help(fatorial)
+
+
+num = int(input('Digite um numero para fatorial: '))
+print(fatorial(num, show=False))
 
 
